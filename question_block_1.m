f6bb1987b4c2e66128c56394f7aa2f0f808df0a7
FerranPares <ferran.pares.pont@gmail.com>
2015-01-02 14:04:14 +0100

    Ferran, the problemseeker

--- question_block_1.m
+++ question_block_1.m
@@ -2,12 +2,17 @@ load('example_dataset_1.mat');
 parameter = 1;
 lambda = 1;
 K = compute_gram_model(data, parameter);
+K2 = L2_distance(data,data,0);
 maxK = max(max(K));
 minK = min(min(K));
 %Eigenvalues
 eigenvalues = eig(K);
 imagesc(K);
+imagesc(K2);
 
 
 [ model, other_values ] = train_dual_kernel_SVM_lambda(data,labels,lambda,K);
 plot_dataset( data, labels, model );
+
+[ model2, other_values2 ] = train_dual_kernel_SVM_lambda(data,labels,lambda,K2);
+plot_dataset( data, labels, model2 );
