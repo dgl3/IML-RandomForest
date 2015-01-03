8c82abd81c0c5d8271366fdb96e4e3b0503a9143
dgl3 <danigibert24@gmail.com>
2015-01-03 11:48:33 +0100

    Compute class frequencies for each K-Fold

--- question_block_3.m
+++ question_block_3.m
@@ -3,9 +3,22 @@ load('example_dataset_1.mat');
 K = 10;
 indexes = create_KFolds(K,data,labels);
 [class1,class2] = compute_class_frequency(labels);
+frequency = class1/size(data,2);
+class1TrainFreq = [];
+class2TrainFreq = [];
+
+class1TestFreq = [];
+class2TestFreq = [];
+
 for i=1:size(indexes,1)
    [XTrain,YTrain,XTest,YTest] = get_partitions(indexes(i,:),data,labels);
    %Compute class frequencey for each K-Fold
    [trainClass1, trainClass2] = compute_class_frequency(YTrain);
+   class1TrainFreq = [class1TrainFreq;trainClass1/size(YTrain,1)];
+   class2TrainFreq = [class2TrainFreq;trainClass2/size(YTrain,1)];
+
    [testClass1, testClass2] = compute_class_frequency(YTest);
+   class1TestFreq = [class1TestFreq;testClass1/size(YTest,1)];
+   class2TestFreq = [class2TestFreq;testClass2/size(YTest,1)];
+
 end
